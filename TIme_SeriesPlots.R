159322f66cec5ee1bb36aed4dbd868c7641003b4
Megan Feddern <mfeddern@users.noreply.github.com>
2024-12-07 10:54:34 -0800

    Updating plots!

--- TIme_SeriesPlots.R
+++ TIme_SeriesPlots.R
@@ -69,19 +69,31 @@ dat_lakewa <- dplyr::rename(dat_lakewa,
 
 
 ### Plots ###
+annotateWA<-data.frame(label=c("Sewage Inputs", "Water Treatment"),
+                       date=c(dat_lakewa$date[50],dat_lakewa$date[200]),
+                       y=c(5,5), TimeSeries=c("TP","TP"))
 lakeWA<- ggplot(data=dat_lakewa, aes(y=Anomalies, x=date, group=TimeSeries,linetype=TimeSeries, col=TimeSeries)) +
   geom_hline(yintercept=0)+
   geom_line(size = 1) +
-  geom_point() +
+  #ylim(c(-2,5))+
+ 
   labs(color = "Time Series")+
   scale_colour_manual(values=c(col[2],col[3]),name="Time Series",labels=c("Cyanobacteria \n (Biological Response)", "Total Phosphorus \n (Ecological Driver)"))+
   scale_linetype_manual(values=c("solid","dashed"),name="Time Series",labels=c("Cyanobacteria \n (Biological Response)", "Total Phosphorus \n (Ecological Driver)"))+
   theme_classic()+
   ylab("Scaled Anomaly")+
   geom_vline(xintercept=as.numeric(dat_lakewa$date[111]), lty=2,col='grey')+
+  geom_text(data=annotateWA, aes(x = date, y = y, 
+                                label = label),  col='black',fontface = "italic")+
   xlab("")
 lakeWA
 
+
+annotateGoA<-data.frame(label=c("High Aleutian \n Low Variability",
+                                "Low Aleutian \n Low Variability",
+                                "Marine \n Heatwaves"),
+                       date=c(1977, 2000,2020),
+                       y=c(3.25,3.25,3.25), Anomalies=c("z_sst","z_sst","z_sst"))
 GoA_TS<- ggplot(data=dat2%>%
                   select(year, salmon_DFA, z_sst)%>%
                   mutate(salmon=scale(salmon_DFA))%>%
@@ -92,28 +104,42 @@ GoA_TS<- ggplot(data=dat2%>%
  # labs(color = "Time Series")+
   scale_linetype_manual(values=c("solid","dashed"), name="Time Series",labels=c("Salmon\n (Biological Response)", "SST\n (Ecological Driver)"))+
   scale_colour_manual(values=c(col[2],col[3]), name="Time Series",labels=c("Salmon\n (Biological Response)", "SST\n (Ecological Driver)"))+
-  geom_vline(xintercept=1988, lty=2,col='grey')+
+  geom_vline(xintercept=c(1988,2014), lty=c(2,2),col='grey')+
   theme_classic()+
+  geom_text(data=annotateGoA, aes(x = date, y = y, 
+                                 label = label),  
+            col='black',fontface = "italic")+
   geom_hline(yintercept=0)+
   ylab("")+
+  ylim(c(-2.5,3.5))+
+  xlim(c(1970,2024))+
   xlab("Year")
 
 GoA_TS
 
-
+annotateLTER<-data.frame(label=c("Zooplankton \n regime",
+                                "Piscivore \n regime",
+                                "Novel \n regime"),
+                        date=c(1992, 2011,2020),
+                        y=c(3.35,3.35,3.35), Dataset=c("Large_scale","Large_scale","Large_scale"))
 LTER<-ggplot(data=analysis_long%>%
          filter(Dataset=="mean_sec_scale"| Dataset=="Large_scale"), aes(y=values, x=year4, group=Dataset,linetype=Dataset, col=Dataset)) +
   geom_hline(yintercept=0)+
   geom_line(size = 1) +
+  ylim(c(-2.5,3.7))+
   geom_point() +
   labs(color = "Time Series")+
   scale_colour_manual(values=c(col[2],col[3]),name="Time Series",labels=c("Large Zooplankton \n (Biological Response)", "Water Clarity \n (Ecological Driver)"))+
   scale_linetype_manual(values=c("solid","dashed"),name="Time Series",labels=c("Large Zooplankton \n (Biological Response)", "Water Clarity \n (Ecological Driver)"))+
    theme_classic()+
+  geom_text(data=annotateLTER, aes(x = date, y = y, 
+                                  label = label),  
+            col='black',fontface = "italic")+
   ylab("")+
   geom_vline(xintercept=2007, lty=2,col='grey')+
-  geom_vline(xintercept=2014, lty=2,col='grey')+
+  geom_vline(xintercept=2014.5, lty=2,col='grey')+
   xlab("")
+LTER
 
 ggarrange(LTER, lakeWA, GoA_TS, labels=c("A.", "B.", "C."), nrow=3)
 ggsave("Figure_1_timeseries.png", height = 8, width = 7)
